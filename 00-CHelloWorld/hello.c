c640c74b8cb1e3f72e46e8ce0a1709929c1b54a2
ebaltazar <38117838+ebaltazar@users.noreply.github.com>
2021-04-11 20:22:18 -0300

    Update hello.c

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,3 +1,10 @@
+/* Hello World!
+ * Para compilar y generar salida: gcc -Wall -std=c11 hello.c -o -output.txt
+ * Al generar el archivo output.txt arroja el siguiente mensaje dentro: 'This program cannot be run in DOS mode'
+ * Por lo que luego de generarlo utilizo: output.txt > salida.txt
+ * No pude encontrar forma de generarlo directamente.
+ */
+
 #include <stdio.h>
 #include <conio.h>
 
